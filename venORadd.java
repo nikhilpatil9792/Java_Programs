cf635d5bc147eccc473780ad23a273852cb47ab4
java_programs <np8612122@gmail.com>
2025-02-24 20:31:07 +0530

    java file added

---
+++ venORadd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class evenORadd
+{
+     int no;
+     Scanner np= new Scanner(System.in);
+     System.out.println("Enter the no");
+     no=np.nextInt();
+
+     if(no%2==0)
+     {
+        System.out.println("No is Even");
+     }
+    else
+    {
+             System.out.println("No is Odd");
+    }
+}
